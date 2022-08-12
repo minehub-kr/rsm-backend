264775c48edd931fd3983942e5aa5bbf36f6df5a
Alex4386 <psh010209@gmail.com>
2022-08-12 14:42:52 +0000

    chore: pm2 configuration mismatch

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ dotenv.config({ path: path.join(__dirname, '.env') });
 module.exports = {
   apps: [
     {
-      name: 'MCSV API',
+      name: 'MinehubRSM',
       cwd: '.',
       script: 'yarn',
       args: ['start'],
@@ -26,7 +26,7 @@ module.exports = {
       user: process.env.DEPLOY_PRODUCTION_USER,
       host: process.env.DEPLOY_PRODUCTION_HOST,
       ref: 'origin/main',
-      repo: 'git@github.com:mcsv-kr/mcsv-api.git',
+      repo: 'git@github.com:minehub-kr/minehub-rsm.git',
       path: process.env.DEPLOY_PRODUCTION_PATH,
       'pre-deploy-local': `scp -Cr ./.env ${process.env.DEPLOY_PRODUCTION_USER}@${process.env.DEPLOY_PRODUCTION_HOST}:${process.env.DEPLOY_PRODUCTION_PATH}/current`,
       'post-deploy': `yarn && yarn prisma generate && yarn build && pm2 startOrRestart ecosystem.config.js`,
